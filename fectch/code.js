c48d5d9c50a04e41213bbc46498fab668d866c52
levlups <52762684+levlups@users.noreply.github.com>
2022-07-14 12:42:25 -0400

    Update code.js

--- fectch/code.js
+++ fectch/code.js
@@ -3,18 +3,18 @@ async function postData(url = '', data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-mode: 'no-cors', // no-cors
+//mode: 'no-cors', // no-cors
     headers: {
     //  'Content-Type': 'application/json'
        'Content-Type': 'application/x-www-form-urlencoded',
-    },
+    }
   
-    body: "nplayer=michael" // body data type must match "Content-Type" header
+  //  body: "nplayer=michael" // body data type must match "Content-Type" header
   });
   return response.text(); // parses JSON response into native JavaScript objects
 }
 
-postData('https://galacticau.fun/postLuna.php', { answer: 42 })
+postData('https://galacticau.fun/postLuna.php/?nplayer=michael', { answer: 42 })
   .then(data => {
     console.log(data); // JSON data parsed by `data.json()` call
   });
