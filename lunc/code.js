cad7a10a3cdfcf6a868f629cb919b14f55f0164d
levlups <52762684+levlups@users.noreply.github.com>
2022-07-14 19:06:07 -0400

    Update code.js

--- lunc/code.js
+++ lunc/code.js
@@ -47,7 +47,7 @@ xhr.onreadystatechange = function() { // Call a function when the state changes.
 
 var cool=JSON.parse(xhr.response).txs[0].raw_log
 
-console.log(JSON.parse(cool)[0].events[0])
+console.log(JSON.parse(cool)[0].events[0].attributes[0])
       
      // var transactions=JSON.parse(xhr.response).txs
       
