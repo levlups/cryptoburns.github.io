38628bcc046a751183db0ce5e846e0ecc4b19e8f
levlups <52762684+levlups@users.noreply.github.com>
2022-07-14 12:10:23 -0400

    Update code.js

--- fectch/code.js
+++ fectch/code.js
@@ -9,9 +9,9 @@ async function postData(url = '', data = {}) {
        'Content-Type': 'application/x-www-form-urlencoded',
     },
   
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
+    body: "nplayer=michael" // body data type must match "Content-Type" header
   });
-  return response.json(); // parses JSON response into native JavaScript objects
+  return response.text(); // parses JSON response into native JavaScript objects
 }
 
 postData('https://galacticau.fun/postLuna.php', { answer: 42 })
