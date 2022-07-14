906460256ff6cd1e07edb8aa0403d0c924a62c5a
levlups <52762684+levlups@users.noreply.github.com>
2022-07-14 12:11:59 -0400

    Update code.js

--- fectch/code.js
+++ fectch/code.js
@@ -3,7 +3,7 @@ async function postData(url = '', data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-  
+  mode: 'no-cors', // no-cors
     headers: {
     //  'Content-Type': 'application/json'
        'Content-Type': 'application/x-www-form-urlencoded',
