ca4a1863aa7a8d607ec7a2820c3bc8fca4935839
levlups <52762684+levlups@users.noreply.github.com>
2022-07-14 13:57:52 -0400

    Update code.js

--- epoch/code.js
+++ epoch/code.js
@@ -11,3 +11,17 @@ function timeConverter(UNIX_timestamp){
   return time;
 }
 console.log(timeConverter(8888888));
+document.getElementById("go").innerHtml=timeConverter(8888888)
+
+const dateStr = '2022-04-27';
+
+const date = new Date(dateStr);
+
+// 👇️ timestamp in milliseconds
+const timestampInMs = date.getTime();
+
+// 👇️ timestamp in seconds (Unix timestamp)
+const timestampInSeconds = Math.floor(date.getTime() / 1000);
+console.log(timestampInSeconds);
+
+
