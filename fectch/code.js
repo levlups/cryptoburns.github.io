dc05b1fcc00f05d19923ec401661911962b6cc9d
levlups <52762684+levlups@users.noreply.github.com>
2022-07-14 12:51:16 -0400

    Update code.js

--- fectch/code.js
+++ fectch/code.js
@@ -1,20 +1,12 @@
-// Example POST method implementation:
-async function go(){
-  // Default options are marked with *
-  const response = await fetch('https://galacticau.fun/postLuna.php/?nplayer=michael', {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-//mode: 'no-cors', // no-cors
-    headers: {
-    //  'Content-Type': 'application/json'
-       'Content-Type': 'application/x-www-form-urlencoded',
-    }
+
+fetch('https://galacticau.fun/postLuna.php/?nplayer=michael', {"nplayer":"jjjjj"})
+.then(data => {
+    // Do some stuff here
   
-  //  body: "nplayer=michael" // body data type must match "Content-Type" header
-  });
-  return response.text(); // parses JSON response into native JavaScript objects
-}
-
-go();
-
+  console.log(data)
+})
+.catch(err => {
+    // Catch and display errors
+})
 
 
