f59b205920fe421432212140003159c7f4144665
levlups <52762684+levlups@users.noreply.github.com>
2022-07-15 15:16:10 -0400

    Update code.js

--- fun/code.js
+++ fun/code.js
@@ -6,7 +6,10 @@ async function fetchMovies() {
   const response = await fetch(url,{
                               mode: 'no-cors',
         credentials: 'include',
-        method: 'POST'
+        method: 'POST',
+    headers: {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+  }
                                });
   // waits until the request completes...
   console.log(response);
