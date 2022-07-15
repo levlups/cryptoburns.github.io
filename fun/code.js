41f6c03cebd1cdb7adee12bfdafabc7e172f7f25
levlups <52762684+levlups@users.noreply.github.com>
2022-07-15 13:34:30 -0400

    Update code.js

--- fun/code.js
+++ fun/code.js
@@ -1,1 +1,11 @@
 console.log("life is beautiful")
+let url="https://galacticau.fun/postLuna.php"
+fetch(url)
+    .then(response => {
+  console.log(response.text());
+        // handle the response
+    })
+    .catch(error => {
+   console.log(error);
+        // handle the error
+    });
