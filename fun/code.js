cd73d10340560c3e51b628c13697a0c9eb5b5488
levlups <52762684+levlups@users.noreply.github.com>
2022-07-15 15:11:00 -0400

    Update code.js

--- fun/code.js
+++ fun/code.js
@@ -3,7 +3,11 @@ let url="https://galacticau.fun/postLuna.php/?nplayer=fud"
 
 
 async function fetchMovies() {
-  const response = await fetch(url);
+  const response = await fetch(url{
+                              mode: 'cors',
+        credentials: 'include',
+        method: 'POST'
+                               });
   // waits until the request completes...
   console.log(response);
 }
