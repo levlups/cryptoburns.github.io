4f40f024d1603ad75099b0032f916391e4bb9cbc
levlups <52762684+levlups@users.noreply.github.com>
2022-07-15 15:18:25 -0400

    Update code.js

--- fun/code.js
+++ fun/code.js
@@ -2,18 +2,15 @@ console.log("life is beautiful")
 let url="https://galacticau.fun/postLuna.php/?nplayer=fud"
 
 
-async function fetchMovies() {
-  const response = await fetch(url,{
-                              mode: 'no-cors',
-        credentials: 'include',
-        method: 'POST',
-    headers: {
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  }
-                               });
-  // waits until the request completes...
-  console.log(response);
-}
-
-fetchMovies()
+fetch(url, {
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },    
+    body: new URLSearchParams({
+        'nplayer: 'koola',
+        'password': 'Password!',
+        'grant_type': 'password'
+    })
+});
 
