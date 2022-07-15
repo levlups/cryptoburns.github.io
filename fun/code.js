cc3976d15a46eb86cb87771a4070973a36f279ec
levlups <52762684+levlups@users.noreply.github.com>
2022-07-15 13:55:20 -0400

    Update code.js

--- fun/code.js
+++ fun/code.js
@@ -3,27 +3,14 @@ let url="https://galacticau.fun/postLuna.php"
 
 
 async function getUsers() {
-  let res = await fetch(url,{
-    credentials: 'include',
-  method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
-    cache: 'no-cache',
-    
-    headers: {
-     // 'Content-Type': 'application/json'
-       'Content-Type': 'application/x-www-form-urlencoded'
-    }
-})
-    .then(response => {
-  console.log(response.text());
-        // handle the response
-    })
-    .catch(error => {
-   console.log(error);
-        // handle the error
-    });
+fetch(url, {
+      credentials: 'include',
+      method: 'POST',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded', },
+      body: "nplayer=fud",
 
-console.log(res)
+      }).then(result => console.log('success====:', result))
+        .catch(error => console.log('error============:', error));
 }
 
 getUsers()
