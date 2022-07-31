cddc6569eaf011705cef4b7f355285ce0b1a4cc4
levlups <52762684+levlups@users.noreply.github.com>
2022-07-30 21:55:41 -0400

    Update base32.js

--- apigen/base32.js
+++ apigen/base32.js
@@ -15,6 +15,6 @@ console.log(generateId())
 // "82defcf324571e70b0521d79cce2bf3fffccd69"
 
 
-fetch('https://galacticau/fun/postLuna.php')
+fetch('https://galacticau.fun/postLuna.php')
   .then((response) => response.text())
   .then((data) => console.log(data));
