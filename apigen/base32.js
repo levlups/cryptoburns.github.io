f150fb617dd186f8c55bddea6db1bb029c709584
levlups <52762684+levlups@users.noreply.github.com>
2022-07-30 21:43:01 -0400

    Update base32.js

--- apigen/base32.js
+++ apigen/base32.js
@@ -1,12 +1,15 @@
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
-   }
-   return result;
+// dec2hex :: Integer -> String
+// i.e. 0-255 -> '00'-'ff'
+function dec2hex (dec) {
+  return dec.toString(16).padStart(2, "0")
 }
 
-console.log(makeid(5));
+// generateId :: Integer -> String
+function generateId (len) {
+  var arr = new Uint8Array((len || 40) / 2)
+  window.crypto.getRandomValues(arr)
+  return Array.from(arr, dec2hex).join('')
+}
+
+console.log(generateId())
+// "82defcf324571e70b0521d79cce2bf3fffccd69"
