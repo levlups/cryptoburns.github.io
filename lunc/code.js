fb62de6d48728ef45f8a8571847649ec3c8e3cce
levlups <52762684+levlups@users.noreply.github.com>
2023-05-11 19:39:12 -0400

    Update code.js

--- lunc/code.js
+++ lunc/code.js
@@ -49,11 +49,11 @@ xhr.onreadystatechange = function() { // Call a function when the state changes.
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
 
    // document.getElementById("p").innerHTML=xhr.response *** check this out
-     console.log(JSON.parse(xhr.response).txs[0].raw_log)
+  //   console.log(JSON.parse(xhr.response).txs[0].raw_log)
 
 var cool=JSON.parse(xhr.response).txs[0].raw_log
 
-//console.log(JSON.parse(cool)[0].events[0].attributes[0].value)
+console.log(JSON.parse(cool)[0].events[0].attributes[0].value)
       
      // var transactions=JSON.parse(xhr.response).txs
       
