f77353d3992ed71e8fa7d7af4d1958c1e756ca38
levlups <52762684+levlups@users.noreply.github.com>
2023-05-11 15:39:12 -0400

    Update code.js

--- lunc/code.js
+++ lunc/code.js
@@ -36,7 +36,10 @@ xhr.send();
 //console.log("yoooo")
          var xhr = new XMLHttpRequest();
 //xhr.open("GET", "https://api.terrarity.io/wallets/lunaburn", true);
-         xhr.open("GET", "https://columbus-fcd.terra.dev/v1/txs?offset=0&limit=10&account=terra19kwnqcjgej5pl3wttuv5xllpvjrjmy79tt2f9l", true);
+         
+         
+         xhr.open("GET", "https://columbus-fcd.terra.dev/v1/txs?offset=0&limit=10&account=terra1sk06e3dyexuq4shw77y3dsv480xv42mq73anxu", true);
+       //  xhr.open("GET", "https://columbus-fcd.terra.dev/v1/txs?offset=0&limit=10&account=terra19kwnqcjgej5pl3wttuv5xllpvjrjmy79tt2f9l", true);
 //Send the proper header information along with the request
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 xhr.onreadystatechange = function() { // Call a function when the state changes.
